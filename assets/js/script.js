e0075d99bd676689443fa08e27982f6ff0c9d849
Thomas Ciszek <tciszek515@gmail.com>
2022-06-15 16:04:13 -0400

    general idea of some of the functions

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// Functions
+
+// getLocation
+  //grab user search parameter
+  //fetch logitude and latitude using something
+  //http://www.gps-coordinates.net/api/"search parameter"
+  //if response
+    //getCurrentForecast(long,lat)
+    //getFiveDay(long,lat)
+  //else
+    // window.alert(err, place not found)
+
+
+// getCurrentForecast
+  //var apikey =
+  //fetch https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
+  //if response
+    //displayCurrentForecast
+  //else
+    //err
+
+// getFiveDay
+  //var apikey =
+  //fetch https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
+  //if response
+    //displayFiveDay
+  //else
+    //err
+
+
+// displayCurrentForecast
+// displayFiveDay
+// saveData
+// loadData
