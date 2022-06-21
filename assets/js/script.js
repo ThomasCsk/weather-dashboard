c27c2c1ab00f42250c76c689bd0cdf6cb7eb078d
Thomas Ciszek <tciszek515@gmail.com>
2022-06-20 20:00:46 -0400

    save and load functions made. recently searched buttons generated and diplay weather data when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
 var searchForm = document.querySelector('#search-form');
 var searchParameter = document.querySelector('#search-parameter');
-var currentForecastEl = document.querySelector('#current-forecast')
+var currentForecastEl = document.querySelector('#current-forecast');
 var fiveDayDataEl = document.querySelector('#five-day-data');
+var recentSearchesEl = document.querySelector('#recent-searches');
 
 var recentlySearched = [];
+var recentSearchCounter = 0;
 var day = new Date()
 
-var clickHandler = function(event){
+var searchClickHandler = function(event){
   event.preventDefault()
   if(searchParameter.value == ""){
     window.alert('Please enter a location in the search box.')
   }
   else{
-    //saveData(searchParameter.value)
+    saveData(searchParameter.value)
     getLocation(searchParameter.value);
   }
   
 }
+
+var recentlySearchedHandler = function(){
+  getLocation(this.textContent);
+}
+
 var getLocation = function(location){
   var apiKey = 'AjnaBhW02abtslHZhIrbRlAV1U3-RLPBR0l2PUqu6ZnG2LGl0q8h-wr-F1wTDKpm';
   var apiUrl = 'http://dev.virtualearth.net/REST/v1/Locations?query='+location+'&key='+apiKey;
@@ -58,7 +65,6 @@ var getWeatherData = function(lat, lon, location) {
 }
 
 var displayCurrentForecast = function(data, location){
-  console.log(data);
   currentForecastEl.innerHTML = '';
 
   var singleDayEl = document.createElement("div");
@@ -127,12 +133,67 @@ var displayFiveDay = function(data){
   }
 }
 
+var generateRecentlySearched = function(data){
+  recentSearchesEl.innerHTML = '';
+  for (i = 0; i < data.length; i++) {
+    var recentButtonEl = document.createElement('button');
+    recentButtonEl.classList = 'button is-info is-outlined'
+    recentButtonEl.textContent = data[i];
+    recentButtonEl.id = 'recent-' + i;
+
+    recentSearchesEl.appendChild(recentButtonEl);
+    
+    var recentButtonsEl = document.querySelector('#recent-' + i)
+    recentButtonsEl.addEventListener('click', recentlySearchedHandler)
+  }
+  
+}
+
 var saveData = function(location){
+  //Checks the previously searched value and exits the function if the current location is the same as the last
+  var previousLocation = ''
+  previousLocation = localStorage.getItem('location ' + (recentSearchCounter -1), previousLocation);
+  if(previousLocation == location){
+    return;
+  }
 
+  //If the previous location is not the same as the current, the location is saved to the local storage
+  localStorage.setItem('location ' + recentSearchCounter, location);
+  console.log('Location Saved!');
+  recentSearchCounter++;
+  loadData(1);
 }
 
-var loadData = function(){
+var loadData = function(bool){
+  if(bool == 0){
+    var dataArray = [];
+    recentSearchCounter = localStorage.length;
 
+    for (i = 0; i < localStorage.length; i++) {
+      var savedLocation = ''
+      savedLocation = localStorage.getItem('location ' + i, savedLocation);
+
+      dataArray.push(savedLocation);
+    }
+
+    generateRecentlySearched(dataArray);
+  }
+  else if(bool == 1){
+    var savedLocation = ''
+    savedLocation = localStorage.getItem('location ' + (recentSearchCounter - 1), savedLocation);
+
+    var recentButtonEl = document.createElement('button');
+    recentButtonEl.classList = 'button is-info is-outlined'
+    recentButtonEl.textContent = savedLocation;
+    recentButtonEl.id = 'recent-' + (recentSearchCounter - 1);
+
+    recentSearchesEl.appendChild(recentButtonEl);
+    
+    var recentButtonsEl = document.querySelector('#recent-' + (recentSearchCounter - 1))
+    recentButtonsEl.addEventListener('click', recentlySearchedHandler)
+  }
 }
 
-searchForm.addEventListener('submit', clickHandler);
+searchForm.addEventListener('submit', searchClickHandler);
+
+loadData(0)
